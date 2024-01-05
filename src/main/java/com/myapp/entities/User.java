9a8cdf67ba9b7cddf01456599154b78cec9b9251
ELfassiMohamed <elfassimohamed2001@gmail.com>
2024-01-05 20:54:45 +0100

    commit

--- src/main/java/com/myapp/entities/User.java
+++ src/main/java/com/myapp/entities/User.java
@@ -1,10 +1,9 @@
 package com.myapp.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Entity; 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +16,8 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@NotBlank
 	private String name;
-	@NotBlank(message = "Enter a valid email")
 	private String email;
-	@NotBlank
 	private String password;
 
 }
