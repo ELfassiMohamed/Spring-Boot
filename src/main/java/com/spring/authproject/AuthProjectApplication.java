647f81238545872afa7a24abdcd34b98efa5da11
ELfassiMohamed <elfassimohamed2001@gmail.com>
2024-01-29 21:29:35 +0100

    first commit for this project

---
+++ src/main/java/com/spring/authproject/AuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.authproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthProjectApplication.class, args);
+	}
+
+}
