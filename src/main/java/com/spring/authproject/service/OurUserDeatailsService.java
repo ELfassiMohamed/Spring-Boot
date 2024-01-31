460f3f0df57893c2bf19ea29602a3db4c56387eb
ELfassiMohamed <elfassimohamed2001@gmail.com>
2024-01-31 22:17:25 +0100

    commit

---
+++ src/main/java/com/spring/authproject/service/OurUserDeatailsService.java
@@ -0,0 +1,22 @@
+package com.spring.authproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.spring.authproject.repository.ourUsersRepo;
+
+@Service
+public class OurUserDeatailsService implements UserDetailsService{
+
+	@Autowired
+	private ourUsersRepo userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
